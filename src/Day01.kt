7ca6c74bc14b8e5ea4b2027dca7f43f4f2beed2f
Carlos de Blas <carlos.deblascarton@tngtech.com>
2021-12-01 10:09:37 +0100

    day 1

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,22 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        val intList = input.map(String::toInt)
+        return intList.zipWithNext().count { (a, b) -> b > a }
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        val intList = input.map(String::toInt)
+        val windowsSums = intList.windowed(3) { it.sum() }
+        return windowsSums.zipWithNext().count { (a, b) -> b > a }
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    check(part1(testInput) == 7)
+    check(part2(testInput) == 5)
 
     val input = readInput("Day01")
-    println(part1(input))
-    println(part2(input))
+
+    println(part1(input)) // 1759
+    println(part2(input)) // 1805
 }
