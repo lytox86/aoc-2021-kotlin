7cb3834db50af0a8ec40d413c0cb8fb9a14e8c4f
Carlos de Blas <carlos.deblascarton@tngtech.com>
2021-12-02 08:49:31 +0100

    day 2

--- src/Day02.kt
+++ src/Day02.kt
@@ -1,16 +1,47 @@
 fun main() {
+    fun parseCommand(cmd: String, forward: (Int) -> Unit, up: (Int) -> Unit, down: (Int) -> Unit) {
+        val strings = cmd.split(' ')
+        val step = strings[1].toInt()
+        when (strings[0]) {
+            "forward" -> forward(step)
+            "up" -> up(step)
+            "down" -> down(step)
+        }
+    }
+
     fun part1(input: List<String>): Int {
-        return 0
+        var horizontal = 0
+        var depth = 0
+        for (cmd in input) {
+            parseCommand(cmd,
+                forward = { horizontal += it },
+                up = { depth -= it },
+                down = { depth += it })
+        }
+        return horizontal * depth
     }
 
     fun part2(input: List<String>): Int {
-        return 0
+        var aim = 0
+        var depth = 0
+        var horizontal = 0
+        for (cmd in input) {
+            parseCommand(cmd,
+                forward = {
+                    horizontal += it
+                    depth += aim * it
+                },
+                down = { aim += it },
+                up = { aim -= it }
+            )
+        }
+        return horizontal * depth
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day02_test")
-    check(part1(testInput) == 7)
-    check(part2(testInput) == 5)
+    check(part1(testInput) == 150)
+    check(part2(testInput) == 900)
 
     val input = readInput("Day02")
 
