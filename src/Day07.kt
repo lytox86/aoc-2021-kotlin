0290b36f01078b0886f7b6ff20a0f50ce2af7e13
Carlos de Blas <carlos.deblascarton@tngtech.com>
2021-12-07 09:04:27 +0100

    day 7

--- src/Day07.kt
+++ src/Day07.kt
@@ -1,19 +1,30 @@
+import kotlin.math.abs
+import kotlin.math.roundToLong
+
 fun main() {
-    fun part1(input: List<String>): Int {
-        return 0
+    fun t(x: Long): Long = x * (x + 1) / 2
+
+    fun part1(input: List<String>): Long {
+        val nums = input.flatMap { line -> line.splitToSequence(',').map { it.toLong() } }
+        val median = nums.sorted()[nums.size / 2]
+        return nums.sumOf { abs(it - median) }
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part2(input: List<String>): Long {
+        val nums = input.flatMap { line -> line.splitToSequence(',').map { it.toLong() } }
+        val mean = nums.sum().toDouble() / nums.size
+        val mean0 = mean.roundToLong()
+        val mean1 = if (mean0 <= mean) mean0 + 1 else mean0 - 1
+        return minOf(nums.sumOf { t(abs(it - mean0)) }, nums.sumOf { t(abs(it - mean1)) })
     }
 
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day06_test")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+    val testInput = readInput("Day07_test")
+    check(part1(testInput) == 37L)
+    check(part2(testInput) == 168L)
 
-    val input = readInput("Day06")
-    println(part1(input)) // 0
-    println(part2(input)) // 0
+    val input = readInput("Day07")
+    println(part1(input)) // 331067
+    println(part2(input)) // 92881128
 }
---
+++ src/Day08.kt
@@ -0,0 +1,20 @@
+fun main() {
+
+    fun part1(input: List<String>): Long {
+        return 0
+    }
+
+    fun part2(input: List<String>): Long {
+        return 0
+    }
+
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day08_test")
+    check(part1(testInput) == 0L)
+    check(part2(testInput) == 0L)
+
+    val input = readInput("Day08")
+    println(part1(input)) //
+    println(part2(input)) //
+}
