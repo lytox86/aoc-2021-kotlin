48f6dd31be64928dea4a59229fb3eacc6f8dddc9
Carlos de Blas <carlos.deblascarton@tngtech.com>
2021-12-10 09:04:42 +0100

    day 11

--- src/Day10.kt
+++ src/Day10.kt
@@ -1,18 +1,76 @@
 fun main() {
-    fun part1(input: List<String>): Int {
+    val openChars = listOf('<', '(', '{', '[')
+    val closeChars = listOf('>', ')', '}', ']')
+    val corruptedScores = mapOf(
+        ')' to 3,
+        ']' to 57,
+        '}' to 1197,
+        '>' to 25137
+    )
+    val missingScores = mapOf(
+        ')' to 1,
+        ']' to 2,
+        '}' to 3,
+        '>' to 4
+    )
+
+    fun getChunkClosingCharFor(openChar: Char): Char {
+        return closeChars[openChars.indexOf(openChar)]
+    }
+
+    fun getCorruptedChunkScore(line: String): Int {
+        val chunkStack = ArrayDeque<Char>()
+        line.toCharArray().forEach { char ->
+            when (char) {
+                in openChars ->
+                    chunkStack.addFirst(getChunkClosingCharFor(char))
+                in closeChars -> {
+                    if (chunkStack.removeFirst() != char) {
+                        return corruptedScores[char] ?: 0
+                    }
+                }
+            }
+        }
         return 0
     }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part1(input: List<String>): Long {
+        return input.sumOf { getCorruptedChunkScore(it) }.toLong()
+    }
+
+    fun getIncompleteScore(line: String): Long {
+        val chunkStack = ArrayDeque<Char>()
+        line.toCharArray().forEach {
+            when (it) {
+                in openChars ->
+                    chunkStack.addFirst(getChunkClosingCharFor(it))
+                in closeChars -> {
+                    if (chunkStack.removeFirst() != it) {
+                        return -1
+                    }
+                }
+            }
+        }
+
+        var score = 0L
+        while (chunkStack.isNotEmpty()) {
+            score *= 5
+            score += missingScores[chunkStack.removeFirst()] ?: 0
+        }
+        return score
+    }
+
+    fun part2(input: List<String>): Long {
+        val scores = input.map { getIncompleteScore(it) }.filter { it != -1L }.sorted()
+        return scores[scores.size / 2]
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day10_test")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+    check(part1(testInput) == 26397L)
+    check(part2(testInput) == 288957L)
 
     val input = readInput("Day10")
-    println(part1(input))
-    println(part2(input))
+    println(part1(input)) // 392421
+    println(part2(input)) // 2769449099
 }
---
+++ src/Day11.kt
@@ -0,0 +1,21 @@
+fun main() {
+    fun part1(input: List<String>): Long {
+        return 0
+    }
+
+0
+
+    fun part2(input: List<String>): Long {
+        return 0
+    }
+
+
+    // test if implementation meets criteria from the description, like:
+    val testInput = readInput("Day11_test")
+    check(part1(testInput) == 0L)
+    check(part2(testInput) == 0L)
+
+    val input = readInput("Day11")
+    println(part1(input)) // 0
+    println(part2(input)) // 0
+}
