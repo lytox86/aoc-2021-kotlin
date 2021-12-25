1d6a1efe3270b9c4f87458512d1f1519826ecd00
Carlos de Blas <carlos.deblascarton@tngtech.com>
2021-12-25 09:27:13 +0100

    day 25

--- src/Day25.kt
+++ src/Day25.kt
@@ -1,18 +1,43 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return 0
-    }
 
-    fun part2(input: List<String>): Int {
-        return 0
+    fun part1(input: List<String>): Int {
+        val data = input.map { it.toList() }
+        val width = data[0].size
+        val height = data.size
+        val size = width * height
+
+        fun Int.east() = this - (this % width) + ((this % width) + 1) % width
+        fun Int.south() = (this + width) % size
+
+        fun solve(): Int {
+            val field = CharArray(size) { i -> data[i / width][i % width] }
+            var moves = 0
+
+            do {
+                val east = field.withIndex().filter { (i, c) -> c == '>' && field[i.east()] == '.' }
+                east.forEach { (i, c) ->
+                    field[i] = '.'
+                    field[i.east()] = c
+                }
+
+                val south = field.withIndex().filter { (i, c) -> c == 'v' && field[i.south()] == '.' }
+                south.forEach { (i, c) ->
+                    field[i] = '.'
+                    field[i.south()] = c
+                }
+
+                moves++
+            } while (east.isNotEmpty() || south.isNotEmpty())
+
+            return moves
+        }
+        return solve()
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day25_test")
-    check(part1(testInput) == 0)
-    check(part2(testInput) == 0)
+    check(part1(testInput) == 58)
 
     val input = readInput("Day25")
-    println(part1(input)) // 0
-    println(part2(input)) // 0
+    println(part1(input)) // 579
 }
